512a9b9459aa90d475c5962ca7bce35b83c3c34f
kenji ogura <githomev40@gmail.com>
2017-09-17 22:25:11 +0900

    debuf

--- scripts/retarget.py
+++ scripts/retarget.py
@@ -10,6 +10,9 @@ print('{} files'.format(len(files)))
 
 ptn = re.compile('^([0-9\-]+)_(\w+)\.png')
 
+if not os.path.exists(path+'3'):
+  os.mkdir(path+'3')
+
 for f in files[:]:
   parts = ptn.findall(f)
   if len(parts[0]) == 2:
@@ -17,10 +20,10 @@ for f in files[:]:
     classn = parts[0][1]
     org_file = path+'/'+f
     if classn == 'automobile' or classn == 'truck':
-      new_file = path+'3/'+number+'_'+classn+'.png'
+      new_file = path+'3/'+number+'_'+'automobile'+'.png'
+      print(" cp {} {}".format(org_file,new_file))
+      shutil.copy(org_file,new_file)
     else:
       new_file = path+'3/'+number+'_'+'background'+'.png'
-    print(" cp {} {}".format(org_file,new_file))
-    shutil.copy(org_file,new_file)
   else:
     print("unsupport filename",f)
